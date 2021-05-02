726516e3767c404dc054ec330b47d9a0d72be94f
elseidy117 <jaisanchit3@gmail.com>
2021-05-02 07:55:37 +0530

    Refactor named_return_variable.go for better readability

--- functions/named_return_variable.go
+++ functions/named_return_variable.go
@@ -3,16 +3,13 @@ package main
 import "fmt"
 
 func namedReturn(a int, b int) (sum, diff, prod int, div float32) {
-	sum = a + b
-	diff = a - b
-	prod = a * b
-	div = float32(a) / float32(b)
+	sum, diff, prod, div = a+b, a-b, a*b, float32(a)/float32(b)
 	return
 }
 
 func main() {
 
-	val1, val2, val3, val4 := namedReturn(20, 10)
+	val1, val2, val3, val4 := namedReturn(30, 20)
 
 	fmt.Printf("The return value are %d, %d, %d, %.2f\n", val1, val2, val3, val4)
 }
