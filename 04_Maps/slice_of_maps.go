0ea7c5b2bd0c497cc7118eaa604e6f83cee9d918
elseidy117 <jaisanchit3@gmail.com>
2021-05-07 19:42:45 +0530

    Add example for a slice of maps

--- 04_Maps/slice_of_maps.go
+++ 04_Maps/slice_of_maps.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// To create map of slice use make in two steps
+// First, to create the outer slice
+// Second, to create individual map in slice
+
 func main() {
 	maps := make([]map[int]int, 5)
 	for idx := range maps {
