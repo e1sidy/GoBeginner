9170f6b92ee2c78ebd993fe471a7dcfc790b6051
elseidy117 <jaisanchit3@gmail.com>
2021-05-08 18:22:05 +0530

    Add example for big package

--- 05_package/big.go
+++ 05_package/big.go
@@ -17,4 +17,17 @@ func main() {
 	ir3 := big.NewRat(1, 1)
 	ir3.Add(ir, ir2)
 	fmt.Printf("The value for addition is: %v\n", ir3)
+
+	// Consider the following example
+	rm := big.NewRat(math.MaxInt64, 1956)
+	rn := big.NewRat(-1956, math.MaxInt64)
+	ro := big.NewRat(19, 56)
+	rp := big.NewRat(1111, 2222)
+	rq := big.NewRat(1, 1)
+	// The order of execution is from left to right
+	// 1- rq = (rm*rn)
+	// 2- rq = rq+ro
+	// 3- rq = rq*rp
+	rq.Mul(rm, rn).Add(rq, ro).Mul(rq, rp)
+	fmt.Printf("Big Rat: %v\n", rq)
 }
