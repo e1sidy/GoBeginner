0ad0f9559e723c61616f095a5a6d1d56b907a2e5
elseidy117 <jaisanchit3@gmail.com>
2021-06-18 09:15:54 +0530

    recover from panicing

---
+++ 09_error_handling/02_recover.go
@@ -0,0 +1,34 @@
+package main
+
+// Recover, built-in-function, allows a program to regain control of a panicking Go routine
+// The recover is only useful when called inside a deferred function
+// It then retrieves the error value passed through the call to panic
+// When used in normal execution, a call to recover will return nil and have no other effect
+// The panic causes the stack to unwind until a deferred recover() is found or the program terminates.
+// It is analogous to the catch block in the Java and .NET languages.
+
+import (
+	"fmt"
+)
+
+func badCall() {
+	panic("bad end")
+}
+
+func test() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf("Panicking %s\n", e)
+		}
+	}()
+
+	badCall()
+	fmt.Println("After bad call")
+}
+
+// Because the panic is recovered, test() completes normally, and the end message Test completed is printed.
+func main() {
+	fmt.Printf("Calling test\r\n")
+	test()
+	fmt.Printf("Test completed\r\n")
+}
