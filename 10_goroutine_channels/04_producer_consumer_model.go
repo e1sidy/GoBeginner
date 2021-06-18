bd4a4c425f5ae09db924c3dfbef4a2818107a4c0
elseidy117 <jaisanchit3@gmail.com>
2021-06-18 19:21:29 +0530

    Producer consumer pattern

---
+++ 10_goroutine_channels/04_producer_consumer_model.go
@@ -0,0 +1,33 @@
+package main
+
+// Pattern-
+// a function makes the channel and return it (so it plays the role of factory function)
+// Inside the function, a lambda function is a called a goroutine
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	suck(pump())
+	time.Sleep(1e9)
+}
+
+func pump() chan int {
+	ch := make(chan int)
+	go func() {
+		for i := 0; ; i++ {
+			ch <- i
+		}
+	}()
+	return ch
+}
+
+func suck(ch chan int) {
+	go func() {
+		for v := range ch {
+			fmt.Println(v)
+		}
+	}()
+}
