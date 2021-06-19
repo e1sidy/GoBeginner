c719d3bb6ca2d3ed1a61ebcb958c562824be5502
elseidy117 <jaisanchit3@gmail.com>
2021-06-19 20:39:16 +0530

    Implemented tcp client

---
+++ 11_Applications/tcp_client_server/client.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Open connection
+	conn, err := net.Dial("tcp", ":3001")
+	if err != nil {
+		// No connection could be made because the target machine actively refused it.
+		log.Println("Error dialing", err.Error())
+		os.Exit(1) // terminate program
+	}
+
+	inputReader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("What is your name?")
+	clientName, _ := inputReader.ReadString('\n')
+
+	trimmedClient := strings.Trim(clientName, "\n")
+
+	// send info to server until Quit:
+	for {
+		fmt.Println("What to send to the server? Type Q to quit.")
+		input, _ := inputReader.ReadString('\n')
+		trimmedInput := strings.Trim(input, "\n")
+
+		fmt.Printf("trimmedInput:--%s--", trimmedInput)
+
+		if trimmedInput == "Q" {
+			return
+		}
+
+		if _, err := conn.Write([]byte(trimmedClient + " says: " + trimmedInput + "\n")); err != nil {
+			log.Println("Error in sending data to server Error:", err.Error())
+		}
+	}
+}
