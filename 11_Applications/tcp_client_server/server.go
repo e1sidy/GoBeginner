d6a8c38ee152206b2f5e163a26c17815180ed81d
elseidy117 <jaisanchit3@gmail.com>
2021-06-19 20:28:19 +0530

    TCP server implemented

---
+++ 11_Applications/tcp_client_server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	fmt.Println("Starting the server ...")
+
+	// create listner
+	listener, err := net.Listen("tcp", "0.0.0.0:3001")
+	if err != nil {
+		log.Fatal("Error listening ", err.Error())
+		os.Exit(1)
+	}
+
+	// listen and accept connections from clients:
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("No connection found error:", err.Error())
+			return
+		}
+		go doServerStuff(conn)
+	}
+}
+
+func doServerStuff(conn net.Conn) {
+	for {
+		buf := make([]byte, 512)
+		if _, err := conn.Read(buf); err != nil {
+			log.Println("Error reading", err.Error())
+			return
+		} else {
+			fmt.Printf("Received data: %v", string(buf))
+		}
+
+	}
+}
